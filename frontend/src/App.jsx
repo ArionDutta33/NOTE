dffd8f409067e7a542a2a34111d3aebc26c9ef3d
Arion Dutta <146577817+ArionDutta33@users.noreply.github.com>
2024-08-04 17:15:50 +0530

    Login in validation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
-import Home from "./Pages/Home/Home.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path={"/dashboard"} element={<Home />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/signup"} element={<Signup />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,58 @@
+import Navbar from "../../Components/Navbar";
+import { Link } from "react-router-dom";
+import PasswordINput from "../../Components/Input/PasswordINput";
+import { useState } from "react";
+import { validateEmail } from "../../Utils/helper";
+Navbar;
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address");
+      return;
+    }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
+    setError("");
+  };
+  return (
+    <>
+      <Navbar />
+      <div className="flex items-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10">
+          <form onSubmit={handleLogin}>
+            <h4 className="text-2xl mb-7">Login</h4>
+
+            <input
+              type="text"
+              placeholder="Email"
+              className="input-box"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <PasswordINput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+            <button className="btn-primary" type="submit">
+              Login
+            </button>
+            <p className="text-sm text-center mt-4">Not registered yet?</p>
+            <Link className="font-medium text-primary underline" to={"/signup"}>
+              {" "}
+              Create an account
+            </Link>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
